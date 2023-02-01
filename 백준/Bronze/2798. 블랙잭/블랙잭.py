86c0ee7308b74d93ee3f643db115b12066161cb5
HyeonSeop So <44566863+sotthang@users.noreply.github.com>
2023-02-01 11:07:23 +0900

    [Bronze II] Title: 블랙잭, Time: 76 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.py
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -1,9 +1,9 @@
 n, m = map(int, input().split())
 card_list = list(map(int, input().split()))
 answer = 0
-for x in range(len(card_list)-2):
-    for y in range(x+1, len(card_list)-1):
-        for z in range(y+1, len(card_list)):
+for x in range(n-2):
+    for y in range(x+1, n-1):
+        for z in range(y+1, n):
             card = card_list[x]+card_list[y]+card_list[z]
             if m-answer >= m-card and card <= m:
                 answer = card
