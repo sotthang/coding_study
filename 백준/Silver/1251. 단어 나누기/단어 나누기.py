dc2d34f0f4e7253dbc8c90aa42c7ba107132b128
HyeonSeop So <44566863+sotthang@users.noreply.github.com>
2023-02-02 17:26:53 +0900

    [Silver V] Title: 단어 나누기, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1251. 단어 나누기/단어 나누기.py
+++ 백준/Silver/1251. 단어 나누기/단어 나누기.py
@@ -1,8 +1,7 @@
 word = input()
 word_len = len(word)
 word_list = []
-for x in range(1, word_len-2):
-    for y in range(x+1, word_len-1):
-        for z in range(y, word_len):
-            word_list.append(word[:x][::-1]+word[x:z][::-1]+word[z:][::-1])
+for x in range(1, word_len-1):
+    for y in range(x+1, word_len):
+        word_list.append(word[:x][::-1]+word[x:y][::-1]+word[y:][::-1])
 print(sorted(word_list)[0])
