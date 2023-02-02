cdc7750c86e5c76d7912aa67e02307197e87cbf1
HyeonSeop So <44566863+sotthang@users.noreply.github.com>
2023-02-02 17:16:55 +0900

    [Bronze I] Title: 오르막길, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2846. 오르막길/오르막길.py
+++ 백준/Bronze/2846. 오르막길/오르막길.py
@@ -2,8 +2,8 @@ n = int(input())
 road_list = list(map(int, input().split()))
 answer = []
 c = 0
-for i in range(len(road_list)-1):
-    if road_list[i] < road_list[i+1] and (i == len(road_list)-2):
+for i in range(n-1):
+    if road_list[i] < road_list[i+1] and (i == n-2):
         c += road_list[i+1] - road_list[i]
         answer.append(c)
     elif road_list[i] < road_list[i+1]:
