6c0eb03fe0ce456dbffceed39c80a1dd1c6fd5a2
HyeonSeop So <44566863+sotthang@users.noreply.github.com>
2023-02-07 17:44:40 +0900

    [Silver III] Title: 킹, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1063. 킹/킹.py
+++ 백준/Silver/1063. 킹/킹.py
@@ -4,18 +4,15 @@ stone_index = [8-int(stone[1]), ord(stone[0])-65]
 dx_dy_dict = {'T':(-1, 0), 'B':(1, 0), 'L':(0, -1), 'R':(0, 1), 'LT':(-1, -1), 'LB':(1, -1), 'RT':(-1, 1), 'RB':(1, 1)}
 for _ in range(int(n)):
     d = input()
-    if (king_index[0] + dx_dy_dict[d][0]) < 0 or (king_index[1] + dx_dy_dict[d][1]) < 0 or (king_index[0] + dx_dy_dict[d][0]) > 7 or (king_index[1] + dx_dy_dict[d][1]) > 7:
-        continue
-    elif (king_index[0] + dx_dy_dict[d][0]) == stone_index[0] and (king_index[1] + dx_dy_dict[d][1]) == stone_index[1]:
-        if (stone_index[0] + dx_dy_dict[d][0]) < 0 or (stone_index[1] + dx_dy_dict[d][1]) < 0 or (stone_index[0] + dx_dy_dict[d][0]) > 7 or (stone_index[1] + dx_dy_dict[d][1]) > 7:
-            continue
+    if 0 <= (king_index[0] + dx_dy_dict[d][0]) < 8 and 0 <= (king_index[1] + dx_dy_dict[d][1]) < 8:
+        if (king_index[0] + dx_dy_dict[d][0]) == stone_index[0] and (king_index[1] + dx_dy_dict[d][1]) == stone_index[1]:
+            if 0 <= (stone_index[0] + dx_dy_dict[d][0]) < 8 and 0 <= (stone_index[1] + dx_dy_dict[d][1]) < 8:
+                king_index[0] += dx_dy_dict[d][0]
+                king_index[1] += dx_dy_dict[d][1]
+                stone_index[0] += dx_dy_dict[d][0]
+                stone_index[1] += dx_dy_dict[d][1]
         else:
             king_index[0] += dx_dy_dict[d][0]
             king_index[1] += dx_dy_dict[d][1]
-            stone_index[0] += dx_dy_dict[d][0]
-            stone_index[1] += dx_dy_dict[d][1]
-    else:
-        king_index[0] += dx_dy_dict[d][0]
-        king_index[1] += dx_dy_dict[d][1]
 print(chr(king_index[1]+65)+str(8-king_index[0]))
 print(chr(stone_index[1]+65)+str(8-stone_index[0]))
