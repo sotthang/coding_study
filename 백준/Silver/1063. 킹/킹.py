2e66b7d513a234b2c2ce4d1e914bdb6ac8561514
HyeonSeop So <44566863+sotthang@users.noreply.github.com>
2023-02-07 11:42:27 +0900

    [Silver III] Title: 킹, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1063. 킹/킹.py
+++ 백준/Silver/1063. 킹/킹.py
@@ -1,7 +1,6 @@
 king, stone, n = map(str, input().split())
 king_index = [8-int(king[1]), ord(king[0])-65]
 stone_index = [8-int(stone[1]), ord(stone[0])-65]
-a_h_dict = {0:'A', 1:'B', 2:'C', 3:'D', 4:'E', 5:'F', 6:'G', 7:'H'}
 dx_dy_dict = {'T':(-1, 0), 'B':(1, 0), 'L':(0, -1), 'R':(0, 1), 'LT':(-1, -1), 'LB':(1, -1), 'RT':(-1, 1), 'RB':(1, 1)}
 for _ in range(int(n)):
     d = input()
