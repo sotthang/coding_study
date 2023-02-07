0929ea7c25a7eb23f5151cfd62734d684b09166c
HyeonSeop So <44566863+sotthang@users.noreply.github.com>
2023-02-07 14:09:42 +0900

    [Silver III] Title: 터렛, Time: 64 ms, Memory: 33376 KB -BaekjoonHub

---
+++ 백준/Silver/1002. 터렛/터렛.py
@@ -0,0 +1,12 @@
+import math
+for _ in range(int(input())):
+    x1, y1, r1, x2, y2, r2 = map(int, input().split())
+    d = math.sqrt((x2-x1)**2 + (y2-y1)**2)
+    if d == 0 and r1 == r2:
+        print(-1)
+    elif d == r1 + r2 or abs(r1-r2) == d:
+        print(1)
+    elif abs(r1-r2) < d < r1+r2:
+        print(2)
+    else:
+        print(0)
