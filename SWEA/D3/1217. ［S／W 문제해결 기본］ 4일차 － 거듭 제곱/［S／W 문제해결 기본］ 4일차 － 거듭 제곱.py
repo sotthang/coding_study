f323c48bc0b9049609d7bbf871080306c10a99ec
HyeonSeop So <44566863+sotthang@users.noreply.github.com>
2023-03-09 18:00:20 +0900

    [D3] Title: [S/W 문제해결 기본] 4일차 - 거듭 제곱, Time: 101 ms, Memory: 42,192 KB -BaekjoonHub

--- SWEA/D3/1217. ［S／W 문제해결 기본］ 4일차 － 거듭 제곱/［S／W 문제해결 기본］ 4일차 － 거듭 제곱.py
+++ SWEA/D3/1217. ［S／W 문제해결 기본］ 4일차 － 거듭 제곱/［S／W 문제해결 기본］ 4일차 － 거듭 제곱.py
@@ -1,4 +1,10 @@
-for test_case in range(1, 11):
-    t = int(input())
+def recursion(n, m):
+    if m == 0 :
+        return 1
+    else:
+        return n * recursion(n, m-1)
+
+for test_case in range(1, 11) :
+    T = int(input())
     n, m = map(int, input().split())
-    print(f'#{test_case} {n**m}')
+    print(f'#{test_case} {recursion(n, m)}')
