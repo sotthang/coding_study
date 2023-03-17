2f8a817493fc5096b9348dc7923dfc689b3a30a2
HyeonSeop So <44566863+sotthang@users.noreply.github.com>
2023-03-17 10:53:21 +0900

    [D2] Title: 파리 퇴치, Time: 109 ms, Memory: 48,548 KB -BaekjoonHub

--- SWEA/D2/2001. 파리 퇴치/파리 퇴치.py
+++ SWEA/D2/2001. 파리 퇴치/파리 퇴치.py
@@ -1,14 +1,14 @@
 T = int(input())
 for test_case in range(1, T + 1):
     n, m = map(int, input().split())
-    li = [list(map(int, input().split())) for _ in range(n)]
+    fly_list = [list(map(int, input().split())) for _ in range(n)]
     answer = 0
     for x in range(n-m+1):
         for y in range(n-m+1):
-            temp = 0
+            fly_sum = 0
             for a in range(m):
                 for b in range(m):
-                    temp += li[x+a][y+b]
-            if answer < temp:
-                answer = temp
+                    fly_sum += fly_list[x+a][y+b]
+            if answer < fly_sum:
+                answer = fly_sum
     print(f'#{test_case} {answer}')
